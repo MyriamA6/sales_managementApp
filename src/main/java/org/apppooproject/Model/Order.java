4bb122a9b5b67ea28241e3bad3b70c55bbe04972
Sabana Suresh <srs.sabana@gmail.com>
2024-11-03 19:15:49 +0100

    Add files via upload

--- src/main/java/org/apppooproject/Model/Order.java
+++ src/main/java/org/apppooproject/Model/Order.java
@@ -1,54 +1,103 @@
-package org.apppooproject.Model;
+package SalesManager;
 
 import java.util.ArrayList;
 import java.util.Date;
-import java.util.Scanner;
+import java.util.List;
 
 public class Order {
-    private static long numberOfInvoices = 0;
-    private long id;
+    private long orderId;
     private long customerId;
-    private int totalPrice;
-    private Date date;
-    private String state;
-    private String shippingAddress;
-    private ArrayList<Product> clothes;
-    private String status;
-    Scanner scan =new Scanner(System.in);
+    private Date orderDate;
+    private double totalPrice;
+    private OrderStatus status; 
+    private List<Product> content; 
 
-    /*public java.sql.Date getDate() {
-        return date;
-    }*/
+    public enum OrderStatus {
+        IN_PROGRESS,
+        CONFIRMED,
+        CANCELED,
+        PROCESSED
+    }
 
-    public int getTotalPrice() {
+    
+    public Order(long orderId, long customerId) {
+        this.orderId = orderId;
+        this.customerId = customerId;
+        this.orderDate = new Date(); 
+        this.status = OrderStatus.IN_PROGRESS;
+        this.content = new ArrayList<>(); 
+        this.totalPrice = 0.0; 
+    }
+
+    
+    public double calculateTotalPrice() {
+    	totalPrice = 0.0; 
+        for (Product product : content) {
+        	totalPrice += product.getPrice();
+        }
         return totalPrice;
+    }
+
+
+    public void validateOrder() {
+        if (status == OrderStatus.IN_PROGRESS) {
+            status = OrderStatus.CONFIRMED;
+        }
+    }
+
+
+    public void cancelOrder() {
+        if (status == OrderStatus.IN_PROGRESS) {
+            status = OrderStatus.CANCELED;
+        }
+    }
+
+
+    public void addProductToOrder(Product product, int quantity) {
+        for (int i = 0; i < quantity; i++) {
+            content.add(product);
+        }
+        calculateTotalPrice(); 
+    }
+
+
+    public static Order makeAnOrder(List<Product> products, Customer customer) {
+        Order order = new Order(/* generate new order ID here */, customer.getId());
+        for (Product product : products) {
+            order.addProductToOrder(product, 1); 
+        }
+        order.calculateTotalPrice(); 
+        return order;
+    }
+
+
+    public void removeProductFromOrder(Product product) {
+        content.remove(product);
+        calculateTotalPrice(); 
+    }
+
+ 
+    public long getOrderId() {
+        return orderId;
     }
 
     public long getCustomerId() {
         return customerId;
     }
 
-    public long getId() {
-        return id;
+    public Date getOrderDate() {
+        return orderDate;
     }
 
-    public static long getNumberOfInvoices() {
-        return numberOfInvoices;
+    public double getTotalPrice() {
+        return totalPrice;
     }
 
-    public String getState() {
-        return state;
-    }
-
-    public String getShippingAddress() {
-        return shippingAddress;
-    }
-
-    public ArrayList<Product> getClothes() {
-        return clothes;
-    }
-
-    public String getStatus() {
+    public OrderStatus getStatus() {
         return status;
+    }
+
+    public List<Product> getContent() {
+        return content;
     }
 }
