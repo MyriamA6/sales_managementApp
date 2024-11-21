30fdceb2d0edadceb2694860e3c25b0f337d61b6
Myriam Achour <msacours@gmail.com>
2024-11-21 23:43:00 +0100

    Ajout d'une base de données embarquée et affichage avec javafx des factures

--- src/main/java/org/apppooproject/Main.java
+++ src/main/java/org/apppooproject/Main.java
@@ -1,7 +1,11 @@
 package org.apppooproject;
 
+import org.apppooproject.DataBaseManagers.DatabaseInitializer;
+
 public class Main {
     public static void main(String[] args) {
+        DatabaseInitializer.initializeDatabase();
+
         MainApp.main(args);
     }
 }
